ec690a2a4bbfa09346b58135e7a957cb07e8b195
azamyar <azamyar75@gmail.com>
2019-09-17 20:59:35 +0500

    Operators programs

---
+++ src/operatorsJava/OperatorsInJava.java
@@ -0,0 +1,12 @@
+package operatorsJava;
+
+public class OperatorsInJava {
+	public static void main(String[]args) {
+		int a =25;
+		int b =26;
+		float c = (float)a/(float)b;
+		System.out.println(c);
+		
+	}
+
+}
